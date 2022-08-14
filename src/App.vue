71655d2557fad050f3bf7a7b15d5231675d0549d
Balazs Kelemen <kelbal@gmail.com>
2022-08-14 11:46:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <DiceGame/>
+</template>
+
+<script>
+import DiceGame from './components/DiceGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DiceGame
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DiceGame.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="gameArea">
+       
+        <div id="player1">{{player1}}</div>
+        <div id="player2">{{player2}}</div>
+        <div id="output" v-if="player1>player2">{{output2}}</div>
+        <div id="output" v-if="player1<player2">{{output3}}</div>
+        <div id="output" v-if="player1==player2">{{output1}}</div>
+
+
+        
+        
+    </div>
+    <button type="button" @click="random(6)">Roll</button>
+</template>
+
+<script>
+export default {
+  name: 'DiceGame',
+  
+  
+  data(){
+    return {
+      player1: 0,
+      player2: 1,
+      output1: "It was a draw",
+      output2: "Player 1 wins",
+      output3: "Player 2 wins",
+      num: 6,
+    
+      }
+  },
+
+
+   methods: {
+      random(){
+            this.player1 = Math.floor(Math.random()*this.num)+1;
+            this.player2 = Math.floor(Math.random()*this.num)+1;
+                  
+      },
+
+
+   }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ #player1, #player2{
+            display: inline-block;
+            width: 200px;
+            height: 140px;
+            border: 1px solid black;
+            padding: 5px;
+            margin: 25px;
+            text-align: center;
+            font-size: 7em;
+
+        }
+
+        #output{
+              text-align: center;
+            font-size: 2em;
+        }
+
+        button{
+            padding: 10px;
+            text-align: center;
+            width: 550px;
+        }
+</style>
